dcb1e41d9381954f8e58d795b34e664e39b9e6d6
shoewhen <wshu07@gmail.com>
2023-12-30 13:02:12 -0500

    this is kinda ugly but whatever

--- Scenes/animations.py
+++ Scenes/animations.py
@@ -637,6 +637,7 @@ class original_triangle(Scene):
         self.wait(2.5)
 
 
+# Find all the angles that staisfy the given equation
 class sin_equals_one_half(Scene):
     def construct(self):
         axes_right = Axes(x_range =[-1.25, 1.25, 0.25], y_range=[-1.25, 1.25, 0.25], x_length=7, y_length=7, axis_config={"include_tip": False, "font_size": 20})
@@ -649,36 +650,30 @@ class sin_equals_one_half(Scene):
 
         self.play(Create(circle_right), Create(circle_left), run_time=1)
         
+        # Rotating radius line on the right axes
         rotation_center = axes_right.c2p(0, 0)
         theta_tracker = ValueTracker(0.01)
-        line1 = Line(axes_right.c2p(0,0), axes_right.c2p(1,0))
-        line_moving = Line(axes_right.c2p(0,0), axes_right.c2p(1,0))
+        line1 = Line(axes_right.c2p(0,0), axes_right.c2p(1,0)).set_opacity(0)
+        line_moving = Line(axes_right.c2p(0,0), axes_right.c2p(1,0)).set_color(BLUE)
         line_ref = line_moving.copy()
         line_moving.rotate(
             theta_tracker.get_value() * DEGREES, about_point=rotation_center
         )
 
-        # self.add(a)
-        # self.play(GrowFromPoint(line1, axes_right.c2p(0,0)), GrowFromPoint(line_moving, axes_right.c2p(0,0)))
-
-        
-
-
-
+        # Rotating radius line on the left axes
         rotation_center_left = axes_left.c2p(0, 0)
         theta_tracker_left = ValueTracker(0.01)
-        line1_left = Line(axes_left.c2p(0,0), axes_left.c2p(1,0))
-        line_moving_left = Line(axes_left.c2p(0,0), axes_left.c2p(1,0))
+        line1_left = Line(axes_left.c2p(0,0), axes_left.c2p(1,0)).set_opacity(0)
+        line_moving_left = Line(axes_left.c2p(0,0), axes_left.c2p(1,0)).set_color(BLUE)
         line_ref_left = line_moving_left.copy()
         line_moving_left.rotate(
             theta_tracker_left.get_value() * DEGREES, about_point=rotation_center_left
         )
 
-        # self.play()
+        # Animates the creation of the radius lines
         self.play(GrowFromPoint(line1, axes_right.c2p(0,0)), GrowFromPoint(line_moving, axes_right.c2p(0,0)), GrowFromPoint(line1_left, axes_left.c2p(0,0)), GrowFromPoint(line_moving_left, axes_left.c2p(0,0)))
 
-
-
+        # Updaters for rotating radius lines
         line_moving.add_updater(
             lambda x: x.become(line_ref.copy()).rotate(
                 theta_tracker.get_value() * DEGREES, about_point=rotation_center
@@ -691,12 +686,49 @@ class sin_equals_one_half(Scene):
             )
         )
 
+        # All the tex and the rest of the frontend
+        y_line_left = DashedLine(axes_left.c2p(-np.sqrt(3)/2, 1/2), axes_left.c2p(0, 1/2)).set_color(GREEN)
+        y_line = DashedLine(axes_right.c2p(np.sqrt(3)/2, 1/2), axes_right.c2p(0, 1/2)).set_color(GREEN)
 
+        equation1 = MathTex(r"\sin\left(\theta\right)=\frac{1}{2}", font_size=30).move_to(np.array([-5, 3, 0]))
+        one_half_left = MathTex(r"\frac{1}{2}", font_size=30, color=GREEN).move_to(axes_left.c2p(0.2, 1/2))
+        one_half = MathTex(r"\frac{1}{2}", font_size=30, color=GREEN).move_to(axes_right.c2p(-0.2, 1/2))
+
+        theta_equals1_left = MathTex(r"\theta=\frac{5\tau}{12}", font_size=30).move_to(np.array([-5, 3, 0]))
+        theta_equals1 = MathTex(r"\theta=\frac{\tau}{12}", font_size=30).move_to(np.array([5, 3, 0]))
+
+        theta_equals2_left = MathTex(r"\theta=\frac{5\tau}{12} + \tau", font_size=30).move_to(np.array([-5, 3, 0]))
+        theta_equals2 = MathTex(r"\theta=\frac{\tau}{12} + \tau", font_size=30).move_to(np.array([5, 3, 0]))
+
+        equation2_left = MathTex(r"\sin\left(\frac{5\tau}{12}+\tau\right)=\frac{1}{2}", font_size=30).move_to(np.array([-5, 3, 0]))
+        equation2 = MathTex(r"\sin\left(\frac{\tau}{12}+\tau\right)=\frac{1}{2}", font_size=30).move_to(np.array([5, 3, 0]))
+
+        theta_equals3_left = MathTex(r"\theta=\frac{5\tau}{12}+k\ \cdot\ \tau", font_size=30).move_to(np.array([-5, 3, 0]))
+        theta_equals3 = MathTex(r"\theta=\frac{\tau}{12}+k\ \cdot\ \tau", font_size=30).move_to(np.array([5, 3, 0]))
+
+        equation3_left = MathTex(r"\sin\left(\frac{5\tau}{12}+k\ \cdot\ \tau\right)=\frac{1}{2}", font_size=30).move_to(np.array([-5, 3, 0]))
+        equation3 = MathTex(r"\sin\left(\frac{\tau}{12}+k\ \cdot\ \tau\right)=\frac{1}{2}", font_size=30).move_to(np.array([5, 3, 0]))
+
+        # Full animation
+        self.play(Write(equation1))
         self.play(theta_tracker.animate.set_value(29.9), theta_tracker_left.animate.set_value(149.9))
+        self.play(GrowFromPoint(y_line_left, axes_left.c2p(-np.sqrt(3)/2, 1/2)), GrowFromPoint(y_line, axes_right.c2p(np.sqrt(3)/2, 1/2)))
+        self.play(Write(one_half_left), Write(one_half))
+        self.play(ReplacementTransform(equation1, theta_equals1_left), Write(theta_equals1))
         self.wait()
-        self.play(theta_tracker.animate.set_value(389.9), theta_tracker_left.animate.set_value(509.9))
+        self.play(FadeOut(y_line), FadeOut(y_line_left), FadeOut(one_half), FadeOut(one_half_left))
+        self.play(theta_tracker.animate.set_value(389.9), theta_tracker_left.animate.set_value(509.9), ReplacementTransform(theta_equals1_left, theta_equals2_left), ReplacementTransform(theta_equals1, theta_equals2))
+        self.play(GrowFromPoint(y_line_left, axes_left.c2p(-np.sqrt(3)/2, 1/2)), GrowFromPoint(y_line, axes_right.c2p(np.sqrt(3)/2, 1/2)))
+        self.play(Write(one_half_left), Write(one_half))
+        self.play(ReplacementTransform(theta_equals2_left, equation2_left), ReplacementTransform(theta_equals2, equation2))
         self.wait()
+        self.play(FadeOut(y_line), FadeOut(y_line_left), FadeOut(one_half), FadeOut(one_half_left))
+        self.play(theta_tracker.animate.set_value(2189.9), theta_tracker_left.animate.set_value(2309.9), ReplacementTransform(equation2_left, theta_equals3_left), ReplacementTransform(equation2, theta_equals3), run_time=5)
+        self.play(GrowFromPoint(y_line_left, axes_left.c2p(-np.sqrt(3)/2, 1/2)), GrowFromPoint(y_line, axes_right.c2p(np.sqrt(3)/2, 1/2)))
+        self.play(Write(one_half_left), Write(one_half))
+        self.play(ReplacementTransform(theta_equals3_left, equation3_left), ReplacementTransform(theta_equals3, equation3))
         # self.play(theta_tracker.animate.increment_value(120))
+        self.wait(2)
 
 
 class three_sin_x(Scene):
