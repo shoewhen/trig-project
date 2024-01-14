12419dba66450b9c42dc6193ee53a4fe45af19e8
shoewhen <wshu07@gmail.com>
2024-01-14 15:39:49 -0500

    FINISHED ALL THE MATH

--- Scenes/graphing_section.py
+++ Scenes/graphing_section.py
@@ -155,11 +155,10 @@ class sin_two_x_minus_tau_over_two(Scene):
         )
         
         function_draft1 = axes.plot(lambda x: np.sin(x), color=RED)
-        function_draft2 = axes.plot(lambda x: np.sin(x - TAU / 4), color=RED)
+        function_draft2 = axes.plot(lambda x: np.sin(x - TAU / 2), color=RED)
         function = axes.plot(lambda x: np.sin(2 * x - TAU / 2), color=RED)
         function_tex1 = MathTex(r"f(x)=\sin\left(x\right)", font_size=30, color=RED).move_to(np.array([-5, 3.25, 0]))
-        function_tex2 = MathTex(r"f(x)=\sin\left(x-\frac{\tau}{4}\right)", font_size=30, color=RED).move_to(np.array([-5, 3.25, 0]))
-        function_tex = MathTex(r"f(x)=\sin\left(2\left(x-\frac{\tau}{4}\right)\right)", font_size=30, color=RED).move_to(np.array([-5, 3.25, 0]))
+        function_tex2 = MathTex(r"f(x)=\sin\left(x-\frac{\tau}{2}\right)", font_size=30, color=RED).move_to(np.array([-5, 3.25, 0]))
         dist_tex = MathTex(r"f(x)=\sin\left(2x-\frac{\tau}{2}\right)", font_size=30, color=RED).move_to(np.array([-5, 3.25, 0]))
 
         self.add(axes)
@@ -168,9 +167,8 @@ class sin_two_x_minus_tau_over_two(Scene):
         self.play(Create(function_draft1), run_time=5)
         self.play(ReplacementTransform(function_tex1, function_tex2))
         self.play(ReplacementTransform(function_draft1, function_draft2))
-        self.play(ReplacementTransform(function_tex2, function_tex))
+        self.play(ReplacementTransform(function_tex2, dist_tex))
         self.play(ReplacementTransform(function_draft2, function))
-        self.play(ReplacementTransform(function_tex, dist_tex))
         self.wait()
 
     def add_x_labels(self):
@@ -196,12 +194,10 @@ class cos_three_x_minus_tau_plus_4(Scene):
         
         function_draft1 = axes.plot(lambda x: np.cos(x), color=GREEN)
         function_draft2 = axes.plot(lambda x: np.cos(x) + 4, color=GREEN)
-        function_draft3 = axes.plot(lambda x: np.cos(x - TAU/3) + 4, color=GREEN)
         function = axes.plot(lambda x: np.cos(3 * x - TAU) + 4, color=GREEN)
         function_tex1 = MathTex(r"f(x)=\cos\left(x\right)", font_size=40, color=GREEN).move_to(np.array([-5, 3.25, 0]))
         function_tex2 = MathTex(r"f(x)=\cos\left(x\right) + 4", font_size=40, color=GREEN).move_to(np.array([-5, 3.25, 0]))
-        function_tex3 = MathTex(r"f(x)=\cos\left(x-\frac{\tau}{3}\right)+4", font_size=40, color=GREEN).move_to(np.array([-4.5, 3.25, 0]))
-        function_tex4 = MathTex(r"f(x)=\cos\left(3\left(x-\frac{\tau}{3}\right)\right)+4", font_size=40, color=GREEN).move_to(np.array([-4.2, 3.25, 0]))
+        function_tex3 = MathTex(r"f(x)=\cos\left(x-\tau\right)+4", font_size=40, color=GREEN).move_to(np.array([-4.5, 3.25, 0]))
         function_tex = MathTex(r"f(x)=\cos\left(3x-\tau\right)+4", font_size=40, color=GREEN).move_to(np.array([-4.5, 3.25, 0]))
 
         self.add(axes)
@@ -212,11 +208,9 @@ class cos_three_x_minus_tau_plus_4(Scene):
         self.play(ReplacementTransform(function_draft1, function_draft2))
         self.wait()
         self.play(ReplacementTransform(function_tex2, function_tex3))
-        self.play(ReplacementTransform(function_draft2, function_draft3))
         self.wait()
-        self.play(ReplacementTransform(function_tex3, function_tex4))
-        self.play(ReplacementTransform(function_draft3, function))
-        self.play(ReplacementTransform(function_tex4, function_tex))
+        self.play(ReplacementTransform(function_tex3, function_tex))
+        self.play(ReplacementTransform(function_draft2, function))
         self.wait()
 
     def add_x_labels(self):
